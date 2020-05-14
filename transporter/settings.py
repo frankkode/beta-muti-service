b7969c127c9b271521c20e0c3efb071d36af3ea8
frankkode <masasimpafra@gmail.com>
2020-05-14 23:28:55 +0200

    add pic

--- transporter/settings.py
+++ transporter/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Build paths inside the project like this: os.path.join(BASE_DIR, ..)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
