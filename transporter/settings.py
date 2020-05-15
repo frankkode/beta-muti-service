e1d22c0ce3bb49c1706f1262ccfe887c1a968cd5
frankkode <masasimpafra@gmail.com>
2020-05-16 00:43:13 +0200

    desactivate

--- transporter/settings.py
+++ transporter/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import os
-import env
+yes
+#import env
 import dj_database_url
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
